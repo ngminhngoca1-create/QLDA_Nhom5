1c8cb968de3c04577cd406fab2e2d8573886cfbc
ngminhngoca1-create <ngminhngoca1@gmail.com>
2025-11-07 12:09:06 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLDA_Nhom5.FormDesign; // Đảm bảo namespace đúng để truy cập FrmDangnhap
+
+namespace QLDA_Nhom5
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Khởi chạy form đăng nhập đầu tiên
+            Application.Run(new FrmDangnhap());
+        }
+    }
+}
